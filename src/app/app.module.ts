85098d9ca4fd8527d3c867c068a3823d052281fb
YaronDavid <urlof_desty91@hotmail.com>
2023-04-12 14:46:12 -0600

    Creatting components and adding bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PipesCommonModule } from './components/pipes-common/pipes-common.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PipesCommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
